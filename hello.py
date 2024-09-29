63c541ecf2daa8759ec4f9e842bbcaef3de3041e
Linda Wang <48342363+LindaWang7@users.noreply.github.com>
2024-09-29 19:02:11 -0400

    added timestamp, finished 1.3 activity

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 # TO RUN
 #1. export FLASK_APP=hello.py
@@ -20,9 +23,16 @@ bootstrap = Bootstrap(app)
 
 
 #======Activity 1.3=========
+# @app.route('/')
+# def index():
+#     date = datetime.datetime.now(datetime.timezone.utc)
+#     return render_template("user.html", current_time=date)
+
 @app.route('/')
 def index():
- return render_template('index.html')
+    date = datetime.datetime.now(datetime.timezone.utc)
+    return render_template("user.html", current_time=date)
+
 @app.route('/user/<name>')
 def user(name):
  return render_template('user.html', name=name)
@@ -33,3 +43,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
  return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
