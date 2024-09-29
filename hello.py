93f8035326cd8845758b07460faead15c7d7a1d9
Linda Wang <48342363+LindaWang7@users.noreply.github.com>
2024-09-29 17:47:43 -0400

    added html template to hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#Example 2.1
+# TO RUN
 #1. export FLASK_APP=hello.py
 #2. flask run
-@app.route('/') 
-def index():
-    return '<h1>Hello World!</h1>'
 
-#Example 2-2. hello.py: Flask application with a dynamic route
+
+#======Example 2.1 & 2.2=========
+# @app.route('/') 
+# def index():
+#     return '<h1>Hello World!</h1>'
+# #Example 2-2. hello.py: Flask application with a dynamic route
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, {}!</h1>'.format(name)
+
+
+#======Activity 1.3=========
+@app.route('/')
+def index():
+ return render_template('index.html')
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
-
+ return render_template('user.html', name=name)
