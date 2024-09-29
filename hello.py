94848c5d7f4c4ae80e36e947a2a6499bd1bd43c2
Linda Wang <48342363+LindaWang7@users.noreply.github.com>
2024-09-29 17:42:11 -0400

    finished example 2.2

--- hello.py
+++ hello.py
@@ -8,3 +8,9 @@ app = Flask(__name__)
 @app.route('/') 
 def index():
     return '<h1>Hello World!</h1>'
+
+#Example 2-2. hello.py: Flask application with a dynamic route
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
